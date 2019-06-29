f45c2f9257eb03ff12b2c1ff5ddf6ef4f3ca5773
Jonathan Gravato <jonathangravato@Jonathans-MacBook-Pro.local>
2019-06-29 17:10:57 -0400

    added provider card and provider card route

--- contacts-distance-app/src/App.js
+++ contacts-distance-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ProviderSearch from './components/ProviderSearch';
 import SearchPromAll from './components/SearchPromAll';
+import ProviderCard from './components/ProviderCard';
 import NotFound from './components/NotFound';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={ProviderSearch} />
           <Route exact path="/promall" component={SearchPromAll} />
+          <Route exact path="/card" component={ProviderCard} />
           <Route component={NotFound} />
         </Switch>
       </div>
