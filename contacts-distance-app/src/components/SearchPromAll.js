e3338bf8972c0cddc4e7737039870961181d39be
Gabe Bravo <gabecbravo@gmail.com>
2019-06-29 16:26:14 -0400

    Added a new route for PromiseAll component

--- contacts-distance-app/src/components/SearchPromAll.js
+++ contacts-distance-app/src/components/SearchPromAll.js
@@ -105,7 +105,7 @@ export default function ProviderSearch() {
           setProviders(a)
         }
       })
-  })
+  }, [])
 
   const callGoogleApi = async (destination, limit) => {
     let isLessThanDistanceLimit = false;
