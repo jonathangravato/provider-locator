d2d7972e643c624141bbbfb39b1728a3cc66ce19
Gabe Bravo <gabecbravo@gmail.com>
2019-07-01 22:40:00 -0400

    Added some styling to the list

--- contacts-distance-app/src/components/ProviderList.js
+++ contacts-distance-app/src/components/ProviderList.js
@@ -1,30 +1,30 @@
 import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
 import Divider from '@material-ui/core/Divider';;
 
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     width: '100%',
-//     maxWidth: 360,
-//     backgroundColor: theme.palette.background.paper,
-//   },
-// }));
+const useStyles = makeStyles(theme => ({
+  wrapper: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    paddingTop: '1rem',
+    paddingBottom: '1.5rem',
+    fontSize: '1.125rem;'
+  }
+}));
 
 export default function SimpleList(props) {
-  // const classes = useStyles();
-  const { zipcode, providerGroup, address, city, index } = props
-  console.log('index', index)
+  const classes = useStyles();
+  const { miles, providerGroup, address, city, zipcode } = props
   return (
     <>
+      <div className={classes.wrapper}>
+        <span>{`miles: ${miles}`}</span>
+        <span>{providerGroup}</span>
+        <span>{address}</span>
+        <span>{city}</span>
+        <span>{zipcode}</span>
+      </div>
     <Divider />
-    <ListItem key={zipcode + `${index}`}>
-      <ListItemText primary={providerGroup} />
-      <ListItemText>{address}</ListItemText>
-      <ListItemText>{city}</ListItemText>
-      <ListItemText>{zipcode}</ListItemText>
-    </ListItem>
     </>
   )
 }
--- contacts-distance-app/src/components/SearchPromAll.js
+++ contacts-distance-app/src/components/SearchPromAll.js
@@ -47,6 +47,12 @@ const useStyles = makeStyles(theme => ({
       },
     },
   },
+  listWrapper: {
+    display: 'flex',
+    width: '80%',
+    margin: 'auto',
+    marginTop: 5
+  },
   paper: {
     padding: theme.spacing(2),
     textAlign: 'center',
@@ -85,53 +91,53 @@ export default function ProviderSearch() {
   const [errors, setErrors] = useState(ERROR_VALUES)
   const [isSearching, setSearchingFlag] = useState(false)
 
-    // Function to fetch Github info of a user.
-    const fetchGoogMapsInfo = async (url, provider) => {
-      const googMapsInfo = await axios(url) // API call to get user info from Github.
-      return {
-        ...provider,
-        miles: googMapsInfo.data.miles
-      }
+  // Function to fetch Github info of a user.
+  const fetchGoogMapsInfo = async (url, provider) => {
+    const googMapsInfo = await axios(url) // API call to get user info from Github.
+    return {
+      ...provider,
+      miles: googMapsInfo.data.miles
     }
+  }
 
-    // Iterates all users and returns their Github info.
-    const fetchProviderDistances = async () => {
-      const requests = MEDICAL_PROVIDERS.map((provider) => {
-        const url = `/api/map-data/${fields.origin}/${provider.address}`
-        return fetchGoogMapsInfo(url, provider) // Async function that fetches the user info.
-          .then((a) => {
-            return a // Returns the user info.
-          })
-      })
-      return Promise.all(requests) // Waiting for all the requests to get resolved.
-    }
+  // Iterates all users and returns their Github info.
+  const fetchProviderDistances = async () => {
+    const requests = MEDICAL_PROVIDERS.map((provider) => {
+      const url = `/api/map-data/${fields.origin}/${provider.address}`
+      return fetchGoogMapsInfo(url, provider) // Async function that fetches the user info.
+        .then((a) => {
+          return a // Returns the user info.
+        })
+    })
+    return Promise.all(requests) // Waiting for all the requests to get resolved.
+  }
 
   const filterProvidersByDistance = () => {
     setErrors(ERROR_VALUES)
     setProviders([])
-    if(fields.origin.length > 5 && fields.limit > 0) {
+    if (fields.origin.length > 5 && fields.limit > 0) {
       setFields(FIELD_VALUES)
       setSearchingFlag(true)
       fetchProviderDistances()
         .then(prs => {
-          if(prs.length > 0){
+          if (prs.length > 0) {
             setProviders(prs)
             setSearchingFlag(false)
           }
         })
-    } else if(fields.origin.length < 5) {
-      setErrors( errors => ({ ...errors, originError: true, originText: 'Enter 5 or more characters' }))
-    } else if(fields.limit === 0 || fields.limit === '') {
-      setErrors( errors => ({ ...errors, limitError: true, limitText: 'Enter at least 1 mile distance' }))
+    } else if (fields.origin.length < 5) {
+      setErrors(errors => ({ ...errors, originError: true, originText: 'Enter 5 or more characters' }))
+    } else if (fields.limit === 0 || fields.limit === '') {
+      setErrors(errors => ({ ...errors, limitError: true, limitText: 'Enter at least 1 mile distance' }))
     }
   }
 
   const renderProviders = () => {
-    const filteredProviders = providers.filter( pr => pr.miles < fields.limit )
-    if(filteredProviders.length > 0) {
+    const filteredProviders = providers.filter(pr => pr.miles < fields.limit)
+    if (filteredProviders.length > 0) {
       return (
         <List>
-          { filteredProviders.map((provider, index) => <ProviderList {...provider} index={index} />) }
+          {filteredProviders.map((provider, index) => <ProviderList key={provider.zipcode + `${index}`} {...provider} />)}
         </List>
       )
     } else if (filteredProviders.length === 0) {
@@ -145,53 +151,68 @@ export default function ProviderSearch() {
   }
 
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>
-            <div>
-              <h3>Providers Distance App</h3>
-              <Grid item xs={12}>
-                <TextField
-                  name="origin"
-                  label="Address"
-                  error={errors.originError}
-                  helperText={errors.originText}
-                  className={classes.addressField}
-                  value={fields.origin}
-                  onChange={changeHandler}
-                  margin="normal"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  name="limit"
-                  label="Mile Limit"
-                  error={errors.limitError}
-                  helperText={errors.limitText}
-                  className={classes.limitField}
-                  value={fields.limit}
-                  onChange={changeHandler}
-                  margin="normal"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <Button className={classes.button} onClick={() => filterProvidersByDistance()}>Search</Button>
-              </Grid>
-              {/* { JSON.stringify(providers.length) } */}
-              {
-                providers.length > 0 && !errors.noProviders ? renderProviders() : null
-              }
-              {
-                isSearching ? <CircularProgress className={classes.progress} /> : null
-              }
-              {
-                errors.noProviders ? <p>No Results Found</p> : null
-              }
-            </div>
-          </Paper>
+    <>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <div>
+                <h3>Providers Distance App</h3>
+                <Grid item xs={12}>
+                  <TextField
+                    name="origin"
+                    label="Address"
+                    error={errors.originError}
+                    helperText={errors.originText}
+                    className={classes.addressField}
+                    value={fields.origin}
+                    onChange={changeHandler}
+                    margin="normal"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    name="limit"
+                    label="Mile Limit"
+                    error={errors.limitError}
+                    helperText={errors.limitText}
+                    className={classes.limitField}
+                    value={fields.limit}
+                    onChange={changeHandler}
+                    margin="normal"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button className={classes.button} onClick={() => filterProvidersByDistance()}>Search</Button>
+                </Grid>
+                {/* { JSON.stringify(providers.length) } */}
+                {
+                  isSearching ? <CircularProgress className={classes.progress} /> : null
+                }
+                {
+                  errors.noProviders ? <p>No Results Found</p> : null
+                }
+              </div>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
+      </div>
+      { providers.length > 0 && !errors.noProviders ?
+        <div className={classes.listWrapper}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>
+                <div>
+                  {
+                    renderProviders()
+                  }
+                </div>
+              </Paper>
+            </Grid>
+          </Grid>
+        </div>
+        : null
+      }
+    </>
   );
 }
